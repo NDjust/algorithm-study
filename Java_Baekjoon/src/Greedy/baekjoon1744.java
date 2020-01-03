6d76d53f5de92a0740ddc41dc5608e454263e2a6
Nathan <skeks463@gmail.com>
2020-01-04 07:18:39 +0900

    Retry Greedy 1744

--- Java_Baekjoon/src/Greedy/baekjoon1744.java
+++ Java_Baekjoon/src/Greedy/baekjoon1744.java
@@ -6,63 +6,12 @@ import java.util.Scanner;
 
 public class baekjoon1744 {
     public static void main(String[] args) {
-        /* 틀림 아래 솔루션 참고해서 다시 풀어보고 해석.
-        * */
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        ArrayList<Integer> plus = new ArrayList<>();
-        ArrayList<Integer> minus = new ArrayList<>();
-
-        for (int i = 0; i < num; i++) {
-            int a = sc.nextInt();
-            if (a <= 0) {
-                minus.add(a);
-            } else if (a > 0){
-                plus.add(a);
-            }
-        }
-
-
-        Collections.sort(plus);
-        Collections.sort(minus);
-
-        int ans = 0;
-
-        for (int i = plus.size() - 1; i > 0; i-=2) {
-            if (plus.get(i) != 1 && plus.get(i - 1) != 1) {
-                ans += (plus.get(i) * plus.get(i - 1));
-                continue;
-            }
-
-            if (plus.get(i - 1) == 1) {
-                ans += plus.get(i);
-                ans += plus.get(i - 1);
-            }
-        }
-
-        if (plus.size() % 2 != 0) {
-            ans += plus.get(0);
-        }
-
-
-        for (int i = minus.size() - 1; i > 0; i-=2) {
-            ans += (minus.get(i) * minus.get(i - 1));
-        }
-
-        if (minus.size() % 2 != 0) {
-            ans += minus.get(0);
-        }
-
-        System.out.println(ans);
-    }
-
-    public static void solution() {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         ArrayList<Integer> plus = new ArrayList<Integer>();
         ArrayList<Integer> minus = new ArrayList<Integer>();
-        int zero = 0;
         int one = 0;
+        int zero = 0;
 
         for (int i = 0; i < n; i++) {
             int x = sc.nextInt();
@@ -101,5 +50,8 @@ public class baekjoon1744 {
         }
 
         System.out.println(ans);
+
     }
+
+
 }
