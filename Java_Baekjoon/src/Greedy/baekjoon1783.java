3b37826f3aefdbfcf5384b88b8e19beb05e49897
Nathan <skeks463@gmail.com>
2020-01-06 07:53:15 +0900

    Add Greedy 1783

---
+++ Java_Baekjoon/src/Greedy/baekjoon1783.java
@@ -0,0 +1,24 @@
+package Greedy;
+
+import java.util.Scanner;
+
+public class baekjoon1783 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int height = sc.nextInt();
+        int width = sc.nextInt();
+
+        if (height == 1) {
+            System.out.println(1);
+        } else if (height == 2) {
+            System.out.println(Math.min(4, (width + 1) / 2));
+        } else {
+            if (width >= 7) {
+                System.out.println(width - 2);
+            } else {
+                System.out.println(Math.min(4, width));
+            }
+        }
+    }
+}
