b034a2185f2f57b3822bfae49581d64c114683f5
Nathan <skeks463@gmail.com>
2020-01-12 16:48:34 +0900

    Add 1780

---
+++ Java_Baekjoon/src/DivideConquer/baekjoon1780.java
@@ -0,0 +1,54 @@
+package DivideConquer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class baekjoon1780 {
+
+    public static boolean same(int[][] a, int x, int y, int n) {
+        for (int i = x; i < x + n; i++) {
+            for (int j = y; j < y + n; j++) {
+                if (a[x][y] != a[i][j]) {
+                    return false;
+                }
+             }
+        }
+        return true;
+    }
+
+    public static void solve(int[][] a, int[] cnt, int x, int y, int n) {
+        if (same(a, x ,y, n)) {
+            cnt[a[x][y] + 1] +=1 ;
+            return;
+        }
+        int m = n / 3;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                solve(a, cnt, x + i * m, y + j * m, m);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n =  Integer.valueOf(br.readLine());
+        int[][] matrix = new int[n][n];
+        int[] cnt = new int[3];
+
+        for (int i = 0; i < n; i++) {
+            String[] line = br.readLine().split(" ");
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = Integer.valueOf(line[j]);
+            }
+        }
+
+        solve(matrix, cnt, 0, 0, n);
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println(cnt[i]);
+        }
+    }
+}
