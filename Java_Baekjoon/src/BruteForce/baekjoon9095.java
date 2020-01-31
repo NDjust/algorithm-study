1397413af74575bab4391d7addb73deb1f7424fe
Nathan <skeks463@gmail.com>
2020-01-31 10:29:55 +0900

    Add Recursive Method

--- Java_Baekjoon/src/BruteForce/baekjoon9095.java
+++ Java_Baekjoon/src/BruteForce/baekjoon9095.java
@@ -21,4 +21,26 @@ public class baekjoon9095 {
             System.out.println(d[sc.nextInt()]);
         }
     }
+
+    public static int go(int count, int sum, int goal) {
+        if (count > 10) {
+            return 0;
+        }
+
+        if(sum > goal) {
+            return 0;
+        }
+
+        if (sum == goal) {
+            return 1;
+        }
+
+        int now = 0;
+
+        for (int i = 1; i < 3; i++) {
+            now += go(count + 1, sum + i, goal);
+        }
+
+        return now;
+    }
 }
