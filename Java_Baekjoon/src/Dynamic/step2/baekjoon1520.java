cd0e15163ef92488492d4e1f3409f1914365c425
Nathan <skeks463@gmail.com>
2020-02-27 08:02:10 +0900

    Add 1520

---
+++ Java_Baekjoon/src/Dynamic/step2/baekjoon1520.java
@@ -0,0 +1,52 @@
+package Dynamic.step2;
+
+import java.util.Scanner;
+
+public class baekjoon1520 {
+    public static int mod = 1000000;
+    public static int[] dx = {0, 0, 1, -1};
+    public static int[] dy = {1, -1, 0, 0};
+
+    public static int go(int[][] a, int[][] d, int x, int y) {
+        int n = a.length;
+        int m = a[0].length;
+        
+        if (x == n - 1 && y == m - 1) {
+            return 1;
+        }
+        
+        if (d[x][y] >= 0) {
+            return d[x][y];
+        }
+        
+        d[x][y] = 0;
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x+dx[i];
+            int ny = y+dy[i];
+
+            if (0 <= nx && nx < n && 0 <= ny && ny < m) {
+                if (a[x][y] > a[nx][ny]) {
+                    d[x][y] += go(a, d, nx, ny);
+                }
+            }
+        }
+        return d[x][y];
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[][] a = new int[n][m];
+        int[][] d = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                a[i][j] = sc.nextInt();
+                d[i][j] = -1;
+            }
+        }
+        System.out.println(go(a, d, 0, 0));
+    }
+}
