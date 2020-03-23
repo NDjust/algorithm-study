1e8ffefa84db87ffcd4b12933d5736a1c5e2dee4
Nathan <skeks463@gmail.com>
2020-03-23 10:36:21 +0900

    Add 1924 DayOfWeek

--- Java_Baekjoon/src/Basic/baekjoon1924.java
+++ Java_Baekjoon/src/Basic/baekjoon1924.java
@@ -11,33 +11,52 @@ public class baekjoon1924 {
     public static void main(String[] args) throws ParseException {
         Scanner sc = new Scanner(System.in);
 
-        int x = sc.nextInt();
-        int y = sc.nextInt();
+        String x = sc.next();
+        String y = sc.next();
 
         String day = "";
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        Date date = dateFormat.parse("2007-01-11");
+        String currDate = "2007-";
+
+        if (x.length() == 1) {
+            currDate += "0" + x;
+        } else {
+            currDate += x;
+        }
+
+        if (y.length() == 1) {
+            currDate += "-0" + y;
+        } else {
+            currDate += "-" + y;
+        }
+
+        Date date = dateFormat.parse(currDate);
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(date);
-
         int dayNum = calendar.get(Calendar.DAY_OF_WEEK);
-
 
         switch (dayNum) {
             case 1:
-                day = "MON";
-            case 2:
-                day = "TUE";
-            case 3:
-                day = "WED";
-            case 4:
-                day = "THU";
-            case 5:
-                day = "FRI";
-            case 6:
-                day = "SAT";
-            case 7:
                 day = "SUN";
+                break;
+            case 2:
+                day = "MON";
+                break;
+            case 3:
+                day = "TUE";
+                break;
+            case 4:
+                day = "WED";
+                break;
+            case 5:
+                day = "THU";
+                break;
+            case 6:
+                day = "FRI";
+                break;
+            case 7:
+                day = "SAT";
+                break;
         }
         System.out.println(day);
     }
