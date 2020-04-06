4626dadc8d8428ec21c6f48ba0dd9c5e66881315
Nathan <skeks463@gmail.com>
2020-04-06 10:46:05 +0900

    Add 2743

---
+++ Java_Baekjoon/src/etc/baekjoon2743.java
@@ -0,0 +1,11 @@
+package etc;
+
+import java.util.Scanner;
+
+public class baekjoon2743 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextLine().length());
+    }
+}
