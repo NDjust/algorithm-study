a64743c812b2c5b388e71d3f29ed65b0b0cbd706
Nathan <skeks463@gmail.com>
2020-04-12 20:22:15 +0900

    Add 11724

---
+++ Java_Baekjoon/src/Graph/baekjoon11724.java
@@ -0,0 +1,57 @@
+package Graph;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class baekjoon11724 {
+
+    static ArrayList<ArrayList<Integer>> map = new ArrayList<>();
+    static boolean visited[];
+    static int count = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i <= n; i++) {
+            map.add(new ArrayList<>());
+        }
+
+
+        visited = new boolean[n+1];
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            map.get(x).add(y);
+            map.get(y).add(x);
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (!visited[i]) {
+                dfs(i);
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+
+    public static void dfs(int v) {
+        visited[v] = true;
+
+        for (int value : map.get(v)) {
+            if (!visited[value]) {
+                dfs(value);
+            }
+
+        }
+    }
+}
