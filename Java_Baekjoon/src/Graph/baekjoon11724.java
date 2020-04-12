ced6f01b0da2f9e9519aa76bb52fbd8143d491fb
Nathan <skeks463@gmail.com>
2020-04-13 08:22:09 +0900

    Review 11725

--- Java_Baekjoon/src/Graph/baekjoon11724.java
+++ Java_Baekjoon/src/Graph/baekjoon11724.java
@@ -8,50 +8,46 @@ import java.util.StringTokenizer;
 
 public class baekjoon11724 {
 
-    static ArrayList<ArrayList<Integer>> map = new ArrayList<>();
-    static boolean visited[];
-    static int count = 0;
+    private static ArrayList<ArrayList<Integer>> node = new ArrayList<>();
+    private static boolean[] visited;
+    private static int visitCount = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
         for (int i = 0; i <= n; i++) {
-            map.add(new ArrayList<>());
+            node.add(new ArrayList<>());
         }
 
-
-        visited = new boolean[n+1];
-
+        visited = new boolean[n + 1];
         for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
+            StringTokenizer stn = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(stn.nextToken());
+            int y = Integer.parseInt(stn.nextToken());
 
-            map.get(x).add(y);
-            map.get(y).add(x);
+            node.get(x).add(y);
         }
 
         for (int i = 1; i <= n; i++) {
             if (!visited[i]) {
-                dfs(i);
-                count++;
+                bfs(i);
+                visitCount++;
             }
         }
-        System.out.println(count);
+
+        System.out.println(visitCount);
     }
 
-    public static void dfs(int v) {
-        visited[v] = true;
+    public static void bfs(int r){
+        visited[r] = true;
 
-        for (int value : map.get(v)) {
-            if (!visited[value]) {
-                dfs(value);
+        for (Integer i : node.get(r)) {
+            if (!visited[i]) {
+                bfs(i);
             }
-
         }
     }
 }
