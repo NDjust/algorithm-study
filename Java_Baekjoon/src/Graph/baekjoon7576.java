7468f131a8c89ae88b63b3ae5fe1ef5e32770bbe
Nathan <skeks463@gmail.com>
2020-04-22 07:17:31 +0900

    solve time over 7576

--- Java_Baekjoon/src/Graph/baekjoon7576.java
+++ Java_Baekjoon/src/Graph/baekjoon7576.java
@@ -1,93 +1,89 @@
 package Graph;
 
-import org.w3c.dom.Node;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
+import java.io.*;
 
 class Point {
     int x;
     int y;
-
-    public Point(int x, int y) {
-        this.x = x;
-        this.y = y;
+    //열, 행
+    public Point(int inX, int inY) {
+        this.x = inX;
+        this.y = inY;
     }
 }
 
 public class baekjoon7576 {
+    static int[][] container;
+    static int[][] move = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    static int N;
+    static int M;
 
-    public static int[][] area;
+    public static void main(String[] args)throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        Queue<Point> queue = new LinkedList<Point>();
+        // Col (x)
+        N = Integer.parseInt(st.nextToken());
+        // Row (y)
+        M = Integer.parseInt(st.nextToken());
 
-    public static boolean[][] visited;
+        //out of range 를 막기 위해 N + 2, M + 2
+        container = new int[M+2][N+2];
 
-    public static int m;
+        Arrays.fill(container[0], -1);
+        Arrays.fill(container[M+1], -1);
 
-    public static int n;
+        //행 : y
+        for(int i = 1; i <= M; i++) {
+            String[] input = in.readLine().split(" ");
+            //토마토가 올수 없는 자리는 -1
+            container[i][0] = -1;
+            container[i][N + 1] = -1;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = br.readLine().trim().split(" ");
-        m = Integer.parseInt(input[0]);
-        n = Integer.parseInt(input[1]);
-
-        area = new int[n][m];
-        Queue<Point> queue = new LinkedList<>();
-
-        for (int i = 0; i < n; i++) {
-            input = br.readLine().trim().split(" ");
-            for (int j = 0; j < m; j++) {
-                area[i][j] = Integer.parseInt(input[j]);
-            }
-        }
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (area[i][j] == 1) {
-                    bfs(i, j);
+            for(int j = 1; j <= N; j++) {
+                int tomato = Integer.parseInt(input[j - 1]);
+                container[i][j] = tomato;
+                //토마토가 위치해 있는곳을 표시
+                if(tomato == 1) {
+                    queue.offer(new Point(j, i));
                 }
             }
         }
 
-        int max = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (area[i][j] == 0) {
-                    System.out.println(-1);
-                    return;
-                }
-                if (max < area[i][j]) {
-                    max = area[i][j];
-                }
-            }
-        }
-        System.out.println(max-1);
+        System.out.println(calcDate(queue));
     }
 
-
-    public static void bfs(int a, int b) {
-        Queue<Point> queue = new LinkedList<>();
-        queue.offer(new Point(a, b));
-
-        int[] dx = {1, 0, -1, 0};
-        int[] dy = {0, 1, 0, -1};
-
-        while (!queue.isEmpty()) {
+    public static int calcDate(Queue<Point> queue) {
+        int max = 0;
+        // N, M에 다다르면 멈춤.
+        while(!queue.isEmpty()) {
             Point p = queue.poll();
 
-            for (int i = 0; i < 4; i++) {
-                int mx = p.x + dx[i];
-                int my = p.y + dy[i];
+            //상하 좌우로 움직이기
+            for(int i = 0; i < 4; i++) {
+                int[] d = move[i];
+                int nextX = p.x + d[0];
+                int nextY = p.y + d[1];
 
-                if (mx >= 0 && mx < n && my < m && my >= 0) {
-                    if ((area[mx][my] == 0 || area[mx][my] > area[p.x][p.y] + 1)) {
-                        area[mx][my] = area[p.x][p.y] + 1;
-                        queue.offer(new Point(mx, my));
-                    }
+                //토마토 발견! && 토마토 아직 안익힘
+                if(container[nextY][nextX] == 0) {
+                    container[nextY][nextX] = container[p.y][p.x] + 1;
+                    queue.offer(new Point(nextX, nextY));
                 }
             }
+
         }
+
+        for(int i = 1; i <= M; i++) {
+            for(int j = 1; j <=N; j++) {
+                //안익힌 토마토가 발견되면 -1
+                if(container[i][j] == 0 ) return -1;
+                max = Math.max(container[i][j], max);
+            }
+        }
+
+        return max - 1;
     }
+
 }
