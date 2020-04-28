3253a0e906842b88b13788610dacccfa3acd0772
Nathan <skeks463@gmail.com>
2020-04-29 08:43:39 +0900

    Add Recursive Method

--- Java_Baekjoon/src/binarySearch/baekjoon10816.java
+++ Java_Baekjoon/src/binarySearch/baekjoon10816.java
@@ -10,6 +10,32 @@ import java.util.StringTokenizer;
 
 public class baekjoon10816 {
 
+    private static int findLowerBound(int start, int end, List<Integer> card, int target) {
+        int mid = (start + end) / 2;
+        if (end <= start) {
+            return start;
+        }
+        if (card.get(mid) >= target) {
+            return findLowerBound(start, mid, card, target);
+        } else {
+            return findLowerBound(mid+1, end, card, target);
+        }
+    }
+
+    private static int findUpperBound(int start, int end, List<Integer> card, int target) {
+        int mid = (start + end) / 2;
+
+        if (end <= start) {
+            return start;
+        }
+
+        if (card.get(mid) <= target) {
+            return findUpperBound(mid+1, end, card, target);
+        } else {
+            return findUpperBound(start, mid, card, target);
+        }
+    }
+
     private static int findLowerBound(List<Integer> card, int target) {
         int start = 0;
         int end = card.size();
@@ -43,6 +69,10 @@ public class baekjoon10816 {
         return start;
     }
 
+    private static int totalCount(int start, int end, List<Integer> card, int target) {
+        return findUpperBound(start, end, card, target) - findLowerBound(start, end, card, target);
+    }
+
     private static int getCount(List<Integer> card, int target) {
         return findUpperBound(card, target) - findLowerBound(card, target);
     }
@@ -63,7 +93,7 @@ public class baekjoon10816 {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < m; i++) {
             int value = Integer.parseInt(st.nextToken());
-            sb.append(getCount(card, value)).append(" ");
+            sb.append(totalCount(0, card.size(), card, value)).append(" ");
         }
         System.out.println(sb);
     }
