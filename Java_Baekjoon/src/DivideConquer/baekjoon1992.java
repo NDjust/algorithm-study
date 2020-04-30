054878f3d3a6f274e72afc2cb3e8612ceaea7b35
Nathan <skeks463@gmail.com>
2020-04-30 11:36:22 +0900

    Review 1992

--- Java_Baekjoon/src/DivideConquer/baekjoon1992.java
+++ Java_Baekjoon/src/DivideConquer/baekjoon1992.java
@@ -1,54 +1,51 @@
 package DivideConquer;
 
-import javax.imageio.IIOException;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 
 public class baekjoon1992 {
 
-    public static int[][] a = new int[100][100];
+    private static int[][] video;
 
-    public static boolean same(int x, int y, int n) {
-        for (int i = x; i < x + n; i++) {
-            for (int j = y; j < y + n; j++) {
-                if (a[x][y] != a[i][j]) {
+    private static boolean isSame(int x, int y, int n, StringBuilder sb) {
+        for (int i = x; i < x+n; i++) {
+            for (int j = y; j < y+n; j++) {
+                if (video[x][y] != video[i][j]) {
                     return false;
                 }
             }
         }
+        sb.append(video[x][y]);
         return true;
     }
 
-    public static void solve(int x, int y, int n) {
-        if (same(x, y, n)) {
-            System.out.printf("%d", a[x][y]);
-        } else {
-            System.out.printf("(");
-            int m = n / 2;
-
-            for (int i = 0; i < 2; i++) {
-                for (int j = 0; j < 2; j++) {
-                    solve(x + m * i, y + m * i, m);
-                }
-            }
-            System.out.printf(")");
+    private static void compress(int x, int y, int n, StringBuilder sb) {
+        if (isSame(x, y, n, sb)) {
+            return;
         }
+
+        int m = n / 2;
+        sb.append("(");
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                compress(x+i*m, y+j*m, m, sb);
+            }
+        }
+        sb.append(")");
     }
-
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int n = Integer.parseInt(sc.nextLine());
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        video = new int[n][n];
         for (int i = 0; i < n; i++) {
+            String s = br.readLine();
             for (int j = 0; j < n; j++) {
-                a[i][j] = sc.nextInt();
+                video[i][j] = s.charAt(j) - 48;
             }
         }
-
-        solve(0, 0, n);
-        System.out.println();
-
+        StringBuilder sb = new StringBuilder();
+        compress(0, 0, n, sb);
+        System.out.println(sb);
     }
 }
