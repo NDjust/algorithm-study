a61f92a3be8e67b6e4f132a1d777a0119397e515
Nathan <skeks463@gmail.com>
2020-05-06 11:45:03 +0900

    Short Cut

--- Programmers-java/src/kakao/intern2019/Tuple.java
+++ Programmers-java/src/kakao/intern2019/Tuple.java
@@ -6,75 +6,28 @@ import java.util.stream.Collectors;
 public class Tuple {
 
     public static int[] solution(String s) {
-        String[] split = s.split(",");
-        List<List<Integer>> tuple = new ArrayList<>();
-        List<Integer> nums = new ArrayList<>();
-        StringBuilder num = new StringBuilder();
-        Queue<Character> queue = new LinkedList<>();
+        Set<String> set = new HashSet<>();
+        String[] arr = s.replaceAll("[{]", " ")
+                .replaceAll("[}]", " ")
+                .trim().split(" , ");
+        int[] ans = new int[arr.length];
+        int idx = 0;
 
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) <= 57 && s.charAt(i) >= 48) {
-                queue.add(s.charAt(i));
-            }
+        Arrays.sort(arr, (a, b) -> {
+            return a.length() - b.length();
+        } );
 
-            if (s.charAt(i) == '}' && !queue.isEmpty()) {
-                while (!queue.isEmpty()) {
-                    num.append(queue.poll());
+        for (String s1 : arr) {
+            for (String a : s1.split(",")) {
+                if (set.add(a)) {
+                    ans[idx++] = Integer.parseInt(a);
                 }
-                nums.add(Integer.parseInt(String.valueOf(num)));
-                tuple.add(nums);
-                num = new StringBuilder();
-                nums = new ArrayList<>();
-            }
-
-            if (s.charAt(i) == ',' && !queue.isEmpty()) {
-                while (!queue.isEmpty()) {
-                    num.append(queue.poll());
-                }
-                nums.add(Integer.parseInt(String.valueOf(num)));
-                num = new StringBuilder();
             }
         }
-        List<List<Integer>> sort = sort(tuple);
 
-        List<HashSet> hashSetList = new ArrayList<>();
-        for (List<Integer> list : sort) {
-            HashSet hashSet = new HashSet();
-            hashSet.addAll(list);
-            hashSetList.add(hashSet);
-        }
-        List<Integer> list = new ArrayList<>();
-
-        for (int i = hashSetList.size()-1; i > 0; i--) {
-            hashSetList.get(i).removeAll(hashSetList.get(i-1));
-            Iterator iterator = hashSetList.get(i).iterator();
-
-            while (iterator.hasNext()) {
-                list.add((Integer) iterator.next());
-            }
-        }
-        list.add(sort.get(0).get(0));
-        Collections.reverse(list);
-
-        int[] ans = new int[list.size()];
-
-        for (int i = 0; i < list.size(); i++) {
-            ans[i] = list.get(i);
-        }
         return ans;
     }
 
-    public static List<List<Integer>> sort(List<List<Integer>> tuple) {
-        List<List<Integer>> sortedTuple = new ArrayList<>();
-        for (int i = 0; i < tuple.size(); i++) {
-            for (int j = 0; j < tuple.size(); j++) {
-                if (tuple.get(j).size() == i+1) {
-                    sortedTuple.add(tuple.get(j));
-                }
-            }
-        }
-        return sortedTuple;
-    }
     public static void main(String[] args) {
         solution("{{2},{2,1},{2,1,3},{2,1,3,4}}");
         solution("{{20,111},{111}}");
