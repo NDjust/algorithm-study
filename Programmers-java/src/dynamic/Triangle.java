ed19069406fad4afaa12b0386336f7905d42dd37
Nathan <skeks463@gmail.com>
2020-05-28 09:20:38 +0900

    Triangle

---
+++ Programmers-java/src/dynamic/Triangle.java
@@ -0,0 +1,36 @@
+package dynamic;
+
+import java.util.Arrays;
+
+public class Triangle {
+
+    private static int max = Integer.MIN_VALUE;
+    private static int[][] cache;
+    private static int height;
+
+    public static int solution(int[][] triangle) {
+        height = triangle.length;
+        cache = new int[triangle.length][triangle.length];
+
+        max = rootSum(0, 0, triangle);
+        System.out.println(max);
+        return max;
+    }
+
+    private static int rootSum(int x, int y, int[][] triangle) {
+        if (x == triangle.length) {
+            return 0;
+        }
+
+        if (cache[x][y] != 0) {
+            return cache[x][y];
+        }
+
+        return cache[x][y] = triangle[x][y] + Math.max(rootSum(x+1, y, triangle), rootSum(x+1, y+1, triangle));
+    }
+
+    public static void main(String[] args) {
+        int[][] a = {{7}, {3, 8}, {8, 1, 0}, {2, 7, 4, 4}, {4, 5, 2, 6, 5}};
+        solution(a);
+    }
+}
