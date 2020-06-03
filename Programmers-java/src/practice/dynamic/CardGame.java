e67793843db6df7d1d39e3df8333451ba68dddd4
Nathan <skeks463@gmail.com>
2020-06-03 09:48:41 +0900

    Add BottomUp Code

--- Programmers-java/src/practice/dynamic/CardGame.java
+++ Programmers-java/src/practice/dynamic/CardGame.java
@@ -47,18 +47,19 @@ public class CardGame {
 
 class SolutionBottomUp {
     public int solution(int[] left, int[] right) {
-        int[][] dp = new int[left.length+1][right.length+1];
+        int answer = 0;
+        int[][] dp = new int[left.length + 1][right.length + 1];
 
-        for (int i = left.length - 1; i >= 0; i--) {
-            for (int j = right.length - 1; j >= 0; j--) {
-                if (left[i] > right[j]) {
-                    dp[i][j] = dp[i][j+1] + right[j];
-                } else {
-                    dp[i][j] = Math.max(dp[i+1][j], dp[i+1][j+1]);
+        for(int i = 1; i < left.length + 1; i++) {
+            for(int j = 1; j < right.length + 1; j++) {
+                if(left[left.length - i] > right[right.length - j]) {
+                    dp[i][j] = Math.max(dp[i][j - 1] + right[right.length - j], Math.max(dp[i - 1][j], dp[i - 1][j - 1]));
+                }else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - 1]);
                 }
             }
         }
-
-        return dp[0][0];
+        answer = dp[left.length][right.length];
+        return answer;
     }
 }
