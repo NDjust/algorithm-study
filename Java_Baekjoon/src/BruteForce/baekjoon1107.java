ab475c56269e56dbd3187f2b0c040f70ec8f2e84
Nathan <skeks463@gmail.com>
2020-07-09 10:12:27 +0900

    Review 1107

--- Java_Baekjoon/src/BruteForce/baekjoon1107.java
+++ Java_Baekjoon/src/BruteForce/baekjoon1107.java
@@ -4,62 +4,57 @@ import java.util.Scanner;
 
 public class baekjoon1107 {
 
-    static boolean[] channel = new boolean[10];
+    private static boolean[] brokenChannel = new boolean[10];
 
-    public static int possible(int c) {
-        if (c == 0) {
-            if (channel[0]) {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int targetChannel = Integer.parseInt(sc.nextLine());
+        int m = Integer.parseInt(sc.nextLine());
+        int minClickCount = Math.abs(targetChannel - 100);
+
+        for (int i = 0; i < m; i++) {
+            int channel = sc.nextInt();
+            brokenChannel[channel] = true;
+        }
+
+        for (int channel = 0; channel <= 1_000_000; channel++) {
+            int possibleLen = possibleLen(channel);
+
+            if (possibleLen > 0) {
+                int switchCount = possibleLen + Math.abs(channel - targetChannel);
+
+                if (switchCount < minClickCount) {
+                    minClickCount = switchCount;
+                }
+            }
+        }
+
+
+        System.out.println(minClickCount);
+    }
+
+
+    private static int possibleLen(int num) {
+        if (num == 0) {
+            if (brokenChannel[0]) {
                 return 0;
             } else {
                 return 1;
             }
         }
 
-        int len = 0;
+        int cnt = 0;
 
-        while (c > 0) {
-            if (channel[c % 10]) {
+        while (num > 0) {
+            if (brokenChannel[num % 10]) {
                 return 0;
             }
 
-            len += 1;
-            c /= 10;
-        }
-        return len;
-    }
+            cnt++;
+            num = num / 10;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = Integer.parseInt(sc.nextLine());
-        int m = Integer.parseInt(sc.nextLine());
-
-        for (int i = 0; i < m; i++) {
-            int k = sc.nextInt();
-            channel[k] = true;
         }
 
-        int ans = n - 100;
-
-        if (ans < 0) {
-            ans = -ans;
-        }
-
-        for (int i = 0; i <= 1000000; i++) {
-            int c = i;
-            int len = possible(c);
-
-            if (len > 0) {
-                int press = c - n;
-
-                if (press < 0) {
-                    press = -press;
-                }
-
-                if (ans > len + press) {
-                    ans = len + press;
-                }
-            }
-        }
-        System.out.println(ans);
+        return cnt;
     }
 }
