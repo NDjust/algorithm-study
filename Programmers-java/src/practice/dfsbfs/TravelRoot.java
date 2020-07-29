890bab2d071c195dae5081bbc20612f6ad8e9a38
Nathan <skeks463@gmail.com>
2020-07-29 13:03:08 +0900

    Review

--- Programmers-java/src/practice/dfsbfs/TravelRoot.java
+++ Programmers-java/src/practice/dfsbfs/TravelRoot.java
@@ -4,67 +4,74 @@ import java.util.*;
 
 public class TravelRoot {
 
-    private static boolean[] visited;
-    private static List<Stack<String>> pathList;
-    private static Stack<String> root;
-    public static String[] solution(String[][] tickets) {
-       pathList = new ArrayList<>();
-       visited = new boolean[tickets.length];
-       root = new Stack<>();
-       root.push("ICN");
+    private List<List<String>> travelRoots = new ArrayList<>();
 
-       dfs(tickets, root, 0);
+    private boolean[] useTicket;
 
-       Collections.sort(pathList, new Comparator<Stack<String>>() {
-           @Override
-           public int compare(Stack<String> t1, Stack<String> t2) {
-               for (int i = 0; i < t1.size(); i++) {
-                   if (!t1.get(i).equals(t2.get(i))) {
-                       return t1.get(i).compareTo(t2.get(i));
-                   }
-               }
-               return 0;
-           }
-       });
+    private int ticketCount;
 
-        Stack<String> ans = pathList.get(0);
+    public String[] solution(String[][] tickets) {
+        useTicket = new boolean[tickets.length];
+        ticketCount = tickets.length;
+
+        List<String> root = new ArrayList<>();
+        root.add("ICN");
+        dfs("ICN", root, tickets, 0);
+
+        travelRoots.sort(new Comparator<List<String>>() {
+            @Override
+            public int compare(final List<String> s1, final List<String> s2) {
+                for (int i = 0; i < s1.size(); i++) {
+                    if (!s1.get(i).equals(s2.get(i))) {
+                        return s1.get(i).compareTo(s2.get(i));
+                    }
+
+                }
+                return 0;
+            }
+        });
+
+        List<String> ans = travelRoots.get(0);
         String[] answer = new String[ans.size()];
 
         for (int i = 0; i < ans.size(); i++) {
             answer[i] = ans.get(i);
         }
+
+        System.out.println(Arrays.toString(answer));
         return answer;
     }
 
-    private static void dfs(String[][] tickets, Stack<String> root, int len) {
-        if (len == tickets.length) {
-            Stack<String> result = new Stack<>();
-            result.addAll(root);
-            pathList.add(result);
-            return;
-        };
+    private void dfs(final String prevRoot, final List<String> root, final String[][] tickets, int useTicketCount) {
 
-        String peek = root.peek();
+        if (ticketCount == useTicketCount) {
+            travelRoots.add(new ArrayList<>(root));
+            return;
+        }
 
         for (int i = 0; i < tickets.length; i++) {
-            String start = tickets[i][0];
-            String end = tickets[i][1];
-            if (peek.equals(start) && !visited[i]) {
-                visited[i] = true;
-                root.push(end);
-                dfs(tickets, root, len + 1);
-                visited[i] = false;
-                root.pop();
+            String[] ticket = tickets[i];
+            String arrive = ticket[0];
+            String dest = ticket[1];
+
+
+            if (prevRoot.equals(arrive) && !useTicket[i]) {
+                useTicket[i] = true;
+                root.add(dest);
+                useTicketCount++;
+                dfs(dest, root, tickets, useTicketCount);
+                useTicketCount--;
+                root.remove(root.size() - 1);
+                useTicket[i] = false;
             }
         }
     }
 
     public static void main(String[] args) {
-        String[] solution = solution(new String[][]{{"ICN", "SFO"}, {"ICN", "ATL"}, {"SFO", "ATL"}, {"ATL", "ICN"}, {"ATL", "SFO"}});
+        String[][] tickets = new String[][]{{"ICN", "SFO"}, {"ICN", "ATL"},
+                {"SFO", "ATL"}, {"ATL", "ICN"}, {"ATL", "SFO"}};
 
-        for (String s : solution) {
-            System.out.printf(s + ", ");
-        }
-
+        TravelRoot travelRoot = new TravelRoot();
+        travelRoot.solution(tickets);
     }
 }
