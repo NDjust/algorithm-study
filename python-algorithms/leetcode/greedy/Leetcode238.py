06937c88e57c1e653b1f39c67275c3df313c4667
Nathan <skeks463@gmail.com>
2021-07-19 20:31:49 +0900

    Add leetcode 238 description

--- python-algorithms/leetcode/greedy/Leetcode238.py
+++ python-algorithms/leetcode/greedy/Leetcode238.py
@@ -3,6 +3,15 @@ from typing import List
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        """
+        [1, 2, 3, 4]
+        product except self
+        - (왼쪽의 모든 숫자 곱) * (오른쪽의 모든 숫자 곱)
+        - nums[:i - 1] * nums[i + 1 :]
+
+        self 제외 왼쪽  = nums[i - 1] * (0 ~ i - 1까지의 곱)
+        self 제외 오른쪽  = nums[len(nums)- i - 1] * (len(nums) - 1 ~ i + 1 까지의 곱)
+        """
         ans = []
         left_product = [1] * len(nums)
         right_product = [1] * len(nums)
